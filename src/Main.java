b9e0902411f267f9bb943fdde0231244ea95dda5
Koushik Rai <123098956+koushikrai@users.noreply.github.com>
2023-07-06 22:28:39 +0530

    My First Commit

--- src/Main.java
+++ src/Main.java
@@ -8,47 +8,37 @@ public class Main {
         System.out.println("Enter a Message: ");
         String message = scan.nextLine();
 
-
+        System.out.println();
 
         System.out.print("Type e to encrypt and d to decrypt: ");
         char pick = scan.next().charAt(0);
 
-
+        System.out.println();
 
         System.out.println("Enter number of shifts: ");
         int shift = scan.nextInt();
 
-
+        System.out.println();
 
         String encryptedMessage = "";
 
         for (int i = 0; i < message.length(); i++) {
             char c = message.charAt(i);
 
-            int code = (int) c;
-
             if (pick == 'e') {
-                code += shift;
-                if (code > 'z') {
-                    code -= 26;
-                }
+                c+=shift;
+                if(c>'z') c-=26;
+                encryptedMessage +=c;
             } else if (pick == 'd') {
-                code -= shift;
-                if (code < 'a') {
-                    code += 26;
-                }
+                c-=shift;
+                if(c<'a') c+=26;
+                encryptedMessage += c;
             } else {
-                System.out.println("Invalid!!!!!" +" "+
-                        "Sahi se likhna broo ");
-                return;
+                System.out.println("Invalid input. Please enter 'e' or 'd'.");
             }
-
-            encryptedMessage += (char) code;
         }
-
         System.out.println("Encrypted message: " + encryptedMessage);
-    }
-}
+    }}
 
 
 

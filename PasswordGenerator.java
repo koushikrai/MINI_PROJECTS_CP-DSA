5153aa1fc6eaeed563b936e7830b79805edc8000
Koushik Rai <123098956+koushikrai@users.noreply.github.com>
2023-08-05 21:40:55 +0530

    Update PasswordGenerator.java

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,36 +1,59 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
-public classPasswordGenerator {
+import static java.lang.Math.random;
 
-    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String NUMBERS = "0123456789";
-    private static final String SPECIAL_CHARACTERS = "!@#$%^&*()";
+public class Main {
 
-    public static String generatePassword(int length) {
-        String password = "";
-        Random random = new Random();
-
-        for (int i = 0; i < length; i++) {
-            char character;
-            int type = random.nextInt(3);
-
-            if (type == 0) {
-                character = LETTERS.charAt(random.nextInt(LETTERS.length()));
-            } else if (type == 1) {
-                character = NUMBERS.charAt(random.nextInt(NUMBERS.length()));
-            } else {
-                character = SPECIAL_CHARACTERS.charAt(random.nextInt(SPECIAL_CHARACTERS.length()));
-            }
-
-            password += character;
-        }
-
-        return password;
-    }
+    private static final String SPCHARS = "!@#$%^&*()_+{}[]|:;<>,.?/";
+    private static final String Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String Numbers = "0123456789";
 
     public static void main(String[] args) {
-        String password = generatePassword(10);
-        System.out.println("Your password is: " + password);
+
+
+        Random random = new Random();
+
+        //Taking i/p
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Number of letter to be included :");
+        int NoOFletters = scan.nextInt();
+
+        System.out.println();
+
+        System.out.print("Number of numbers to be included :");
+        int NoOFnums = scan.nextInt();
+
+        System.out.println();
+
+        System.out.print("Number of Sp.Characters to be included :");
+        int NoOFspecialChar = scan.nextInt();
+
+        char [] password = new char[NoOFspecialChar + NoOFnums + NoOFletters];
+        int LENGTH = NoOFspecialChar + NoOFnums + NoOFletters;
+
+        for (int i = 0; i < NoOFspecialChar; i++) {
+            password[i] = SPCHARS.charAt(random.nextInt(SPCHARS.length()));
+        }
+        for (int i = 0; i < NoOFletters; i++) {
+            password[i+NoOFspecialChar] = Letters.charAt(random.nextInt(Letters.length()));
+        }
+        for (int i = 0; i < NoOFnums; i++) {
+            password[i+NoOFletters+NoOFspecialChar] = Numbers.charAt(random.nextInt(Numbers.length()));
+        }
+        for (int i = LENGTH - 1; i >= 0; i--) {
+
+            int j = random.nextInt(i + 1);
+
+            // Swaping password[i]
+            char temp = password[i];
+            password[i] = password[j];
+            password[j] = temp;
+        }
+        System.out.print("Your password is:");
+
+        System.out.println(password);
     }
 }
-
